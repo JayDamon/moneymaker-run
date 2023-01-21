feca2fc2e336863f69af25ddbf0e144b94f53490
jaydamon <Heron2128#>
2023-01-21 12:05:58 -0500

    Update startup script

--- setup-project.sh
+++ setup-project.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-git clone git@github.com:JayDamon/account-service.git ../
-git clone git@github.com:JayDamon/admin-service.git ../
-git clone git@github.com:JayDamon/budget-service.git ../
-git clone git@github.com:JayDamon/http-toolbox.git ../
-git clone git@github.com:JayDamon/moneymaker-api-gateway-service.git ../
-git clone git@github.com:JayDamon/moneymaker-react-client.git ../
-git clone git@github.com:JayDamon/moneymakergocloak.git ../
-git clone git@github.com:JayDamon/plaid-integration.git ../
-git clone git@github.com:JayDamon/transaction-service.git ../
-git clone git@github.com:JayDamon/user-service.git ../
+git clone git@github.com:JayDamon/account-service.git ../account-service
+git clone git@github.com:JayDamon/admin-service.git ../admin-service
+git clone git@github.com:JayDamon/budget-service.git ../budget-serivce
+git clone git@github.com:JayDamon/http-toolbox.git ../http-toolbox
+git clone git@github.com:JayDamon/moneymaker-api-gateway-service.git ../moneymaker-api-gateway-service
+git clone git@github.com:JayDamon/moneymaker-react-client.git ../moneymaker-react-client
+git clone git@github.com:JayDamon/moneymakergocloak.git ../moneymakergocloak
+git clone git@github.com:JayDamon/plaid-integration.git ../plaid-integration
+git clone git@github.com:JayDamon/transaction-service.git ../transaction-service
+git clone git@github.com:JayDamon/user-service.git ../user-service
 
 ./data/setup.sh
